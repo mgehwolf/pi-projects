7a62347f45d5d0f7d68fcea355ca974bf98f0c25
Matthias Gehwolf <34096103+mgehwolf@users.noreply.github.com>
2019-10-21 13:53:27 +0200

    Add files via upload

---
+++ blinky.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <wiringpi.h>
+
+#define LED1 18
+#define LED2 23
+#define LED3 24
+#define LED4 25
+
+void setup() {
+    wiringPiSetupGpio();
+    pinMode(Led1; OUTPUT);
+    pinMode(Led2; OUTPUT);
+    pinMode(Led3; OUTPUT);
+    pinMode(Led4; OUTPUT);
+}
+
+int main(){
+    for(){
+        digitalWrite(LED1, LOW);
+        digitalWrite(LED2, LOW);
+        digitalWrite(LED3, LOW);
+        digitalWrite(LED4, LOW);
+        delay(500);
+        digitalWrite(LED1, HIGH);
+        digitalWrite(LED2, HIGH);
+        digitalWrite(LED3, HIGH);
+        digitalWrite(LED4, HIGH);
+        
+    }
+}
